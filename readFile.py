0846a20352bc094da59d74260240b2c7fa199099
JoyMarocho <joymarocho@gmail.com>
2022-05-24 20:40:07 +0300

    commits

--- readFile.py
+++ readFile.py
@@ -1,7 +1,51 @@
-handle = open("test.txt","r")
+# Reading a file
+# handle = open("test.txt","r")
 
-data = handle.read()
-print(data)
+# data = handle.read()
+# print(data)
 
-handle.close()
+# handle.close()
 
+
+
+# Reading a single line
+# handle = open("test.txt","r")
+
+# data = handle.readline()
+# print(data)
+
+# handle.close()
+
+
+# Reading multiple lines
+# handle = open("test.txt","r")
+
+# data = handle.readlines()
+# print(data)
+
+# handle.close()
+
+# Looping trough a files
+# handle = open("test.txt","r")
+# data = handle.read()
+# counter = 0
+# for word in data.split():
+#     if word == "Python":
+#         counter += 1
+
+# print(counter)
+
+
+
+#  WRITING A FILE
+# handle = open("text-write.txt", "w")
+
+# handle.write("Hello Moringa")
+# handle.close()
+
+
+# Wtinging a text 
+# with operator
+with open("test.txt","r") as handle:
+    data = handle.read()
+    print(data)
